3a64b93a834d44813ecb800a691ac2a9f545c187
manfredy21 <manfredyantonioescobar@gmail.com>
2024-02-09 23:00:23 -0600

    W4 finish

---
+++ scripts/w04-task.js
@@ -0,0 +1,85 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+let myProfile = {
+    name: "Manfredy Escobar",
+    photo: {
+        src: "images/photo.jpg",
+        alt: "My Profile Picture"
+    },
+
+    favoriteFoods: [
+        'Pupusas',
+        'Pizza',
+        'Guacamole',
+        'Salad',
+        'Lasagna'
+    ],
+    
+    hobbies: [
+        'video games',
+        'photografy',
+        'see the stars',
+        'whatch Tv'
+    ],
+
+    placesLived: [],
+};
+/*
+myProfile.placesLived.push(
+    {
+        place: 'Santa Ana, SLV',
+        length: '25 years'
+    }
+);
+
+myProfile.placesLived.push(
+    {
+        place: 'Guadalajara, MX',
+        length: '2 years'
+    }
+);
+*/
+/* Populate Profile Object with placesLive objects */
+
+function addPlace(location, years) {
+    myProfile.placesLived.push({location, years});
+}
+
+addPlace("Santa Ana, El Salvador", "25 Years");
+addPlace("Guadalajara, Mexico", "2 Years");
+
+
+/* DOM Manipulation - Output */
+document.querySelector("#name").innerHTML = myProfile.name;
+
+/* Name */
+
+/* Photo with attributes */
+document.querySelector('#photo').src = myProfile.photo.src
+document.querySelector('#photo').alt = myProfile.photo.alt
+/* Favorite Foods List*/
+myProfile.favoriteFoods.forEach(food => {
+    let li = document.createElement("li");
+    li.textContent = food;
+    document.querySelector("#favorite-foods").appendChild(li);
+});
+
+/* Hobbies List */
+myProfile.hobbies.forEach(item => {
+    let li = document.createElement("li");
+    li.textContent = item;
+    document.querySelector("#hobbies").appendChild(li);
+});
+
+/* Places Lived DataList */
+
+myProfile.placesLived.forEach(place=> {
+    let  dt = document.createElement("dt");
+    dt.textContent = place.location;
+    let dd =  document.createElement("dd");
+    dd.textContent = place.years;
+
+    document.querySelector("#places-lived").appendChild(dt);
+    document.querySelector("#places-lived").appendChild(dd);
+});
